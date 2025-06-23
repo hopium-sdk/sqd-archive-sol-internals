89d598fc657acdc8b42b1c9383924a1fde5f6270
Hopium <hoponhopium@gmail.com>
2025-06-23 16:25:13 +0530

    chore: added status types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./main/types/packed-block";
 export * from "./main/types/sqd-block";
+export * from "./main/types/status.types";
 export * from "./app/compression-provider";
 export * from "./app/s3-provider";
