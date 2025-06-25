51c01c0f34a21def9df1bc594f3fd2f8b9e45a83
Hopium <hoponhopium@gmail.com>
2025-06-25 16:51:29 +0530

    chore: updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./main/types/sqd-block";
 export * from "./main/types/status.types";
 export * from "./app/compression-provider";
 export * from "./app/s3-provider";
+export * from "./app/blocklist-converter";
